9b4e31dd33bf9fad63b583be71320ba33f95a554
sooyoungh <sooyoung.h8@gmail.com>
2022-04-03 00:36:46 +0900

    Start spring boot

---
+++ back/Murange/src/main/java/teamMurange/Murange/MurangeApplication.java
@@ -0,0 +1,13 @@
+package teamMurange.Murange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurangeApplication.class, args);
+	}
+
+}
