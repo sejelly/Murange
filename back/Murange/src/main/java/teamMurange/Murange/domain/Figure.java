023a40b19449f24281b7cd65a5e7169f738c9841
sooyoungh <sooyoung.h8@gmail.com>
2022-04-10 23:51:45 +0900

    Edit domain(Music, Figure, TopDaily)

--- back/Murange/src/main/java/teamMurange/Murange/domain/Figure.java
+++ back/Murange/src/main/java/teamMurange/Murange/domain/Figure.java
@@ -1,5 +1,6 @@
 package teamMurange.Murange.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.Embeddable;
@@ -21,4 +22,38 @@ public class Figure {
     private float disgust;
 
     private float scared;
+
+    // 평균내서 업데이트할 예정, 일단은 변경하는 로직으로 구현(테스트용)
+    public void updateFigure(String updateEmotion, float updateFigure) {
+        
+        switch (updateEmotion) {
+            case "happiness":
+                this.happiness = updateFigure;
+            case "sad":
+                this.sad = updateFigure;
+            case "angry":
+                this.sad = updateFigure;
+            case "neutral":
+                this.sad = updateFigure;
+            case "disgust":
+                this.sad = updateFigure;
+            case "surprised":
+                this.sad = updateFigure;
+            case "scared":
+                this.sad = updateFigure;
+        }
+    }
+
+
+    @Builder
+    public Figure (float happiness, float sad, float angry,float neutral, float surprised, float disgust, float scared) {
+        this.happiness = happiness;
+        this.sad = sad;
+        this.angry = angry;
+        this.neutral = neutral;
+        this.surprised = surprised;
+        this.disgust = disgust;
+        this.scared = scared;
+    }
+
 }
--- back/Murange/src/main/java/teamMurange/Murange/domain/Music.java
+++ back/Murange/src/main/java/teamMurange/Murange/domain/Music.java
@@ -1,5 +1,6 @@
 package teamMurange.Murange.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -32,10 +33,39 @@ public class Music {
     @JoinColumn(name = "category_id")
     private Category category;
 
+//    @ManyToOne
+//    @JoinColumn(name = "playlist_id")
+//    private Playlist playlist;
+
     @OneToMany(mappedBy = "LikeMusic")
     private List<LikeMusic> likeMusics = new ArrayList<>();
 
     @OneToMany(mappedBy = "Enroll")
     private List<Enroll> enrolls = new ArrayList<>();
 
+    // figure 업데이트하기
+    public void updateFigure(Figure figure) {
+        Figure.builder()
+                .angry(figure.getAngry())
+                .disgust(figure.getDisgust())
+                .happiness(figure.getHappiness())
+                .sad(figure.getSad())
+                .neutral(figure.getNeutral())
+                .scared(figure.getScared())
+                .surprised(figure.getSurprised())
+                .build();
+        this.figure = figure;
+    }
+
+    // 테스트용 음악 생성 빌더
+    @Builder
+    public Music(Long id, String title, String singer, Figure figure, int streaming_cnt, Category category) {
+        this.figure = figure;
+        this.id = id;
+        this.title = title;
+        this.singer = singer;
+        this.category = category;
+        this.streaming_cnt = streaming_cnt;
+    }
+    
 }
