bf1d9ca74637eeb4df1028672599b5be3d2368cd
sooyoungh <sooyoung.h8@gmail.com>
2022-05-19 13:13:08 +0900

    Change Controller name

--- back/Murange/src/main/java/teamMurange/Murange/controller/CommentController.java
+++ back/Murange/src/main/java/teamMurange/Murange/controller/CommentController.java
@@ -8,29 +8,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import teamMurange.Murange.domain.Comment;
-import teamMurange.Murange.domain.Music;
 import teamMurange.Murange.dto.CommentRequestDto;
-import teamMurange.Murange.dto.MusicResponseDto;
-import teamMurange.Murange.repository.CommentRepository;
 import teamMurange.Murange.service.CommentService;
 import teamMurange.Murange.service.MusicService;
 
 import java.util.List;
 
-@Api(tags = { "Playlist Controller"})
+@Api(tags = { "Recommendation Controller"})
 @RestController
 @RequiredArgsConstructor
-public class PlaylistPage {
+public class CommentController {
 
     private final CommentService commentService;
     private final MusicService musicService;
-
-    @ApiOperation(value = "플레이리스트별 음악 조회", notes = "플레이리스트의 음악 불러오기")
-    @GetMapping("/playlist/{playlist-id}")
-    public ResponseEntity getMusic(@PathVariable(value = "playlist-id") Long playlistId) {
-        List<MusicResponseDto> musicList = musicService.getMusicsByPlaylist(playlistId);
-        return new ResponseEntity(musicList, HttpStatus.OK);
-    }
 
     @ApiOperation(value = "댓글 조회", notes = "댓글 조회하기")
     @GetMapping("/comments/{playlist-id}")
@@ -45,7 +35,7 @@ public class PlaylistPage {
         commentService.createComment(commentRequestDto);
         return new ResponseEntity(HttpStatus.OK);
     }
-    
+
     @ApiOperation(value = "댓글 수정", notes = "댓글 수정하기")
     @PatchMapping("/comments/{comments-id}")
     public ResponseEntity updateComment(
